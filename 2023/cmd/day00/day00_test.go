a3f03595fc0e896149d867cab11f51a5f6cd3e3f
Bobby Philip <bobbyphilip@gmail.com>
2023-12-02 00:46:03 +1100

    template

--- 2023/cmd/day00/day00_test.go
+++ 2023/cmd/day00/day00_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func TestS2(t *testing.T) {
+	res := Solve2("0.data")
+	if res != 42 {
+		t.Errorf(" failed was %d expected %d", res, 42)
+	}
+}
 func BenchmarkS1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Solve1("0.data")
